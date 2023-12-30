400e69702186dab70f494f07ed3a0ff5d0a32916
emrverskn <140524629+emrverskn@users.noreply.github.com>
2023-12-30 21:15:31 +0300

    Update bookstore-api.py

--- bookstore-api.py
+++ bookstore-api.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # Configure sqlite database
 app.config['MYSQL_DATABASE_HOST'] = 'database'
-app.config['MYSQL_DATABASE_USER'] = '****'
+app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = '****'
 app.config['MYSQL_DATABASE_DB'] = 'bookstore_db'
 app.config['MYSQL_DATABASE_PORT'] = 3306

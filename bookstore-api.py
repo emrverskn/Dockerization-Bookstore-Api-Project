40ec461ffb82d525638736834cd481f38f652bd1
Elif Merve Eriskin <140524629+emrverskn@users.noreply.github.com>
2024-01-12 20:50:46 +0300

    Updated bookstore-api.py

--- bookstore-api.py
+++ bookstore-api.py
@@ -3,7 +3,6 @@ from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 
-# Configure sqlite database
 app.config['MYSQL_DATABASE_HOST'] = 'database'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = '****'

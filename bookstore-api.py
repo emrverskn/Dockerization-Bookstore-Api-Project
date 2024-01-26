5234c2ae6a1eaab3d402bbb411c6dd9fe562b789
Elif Merve Eriskin <140524629+emrverskn@users.noreply.github.com>
2024-01-26 22:10:54 +0300

    Update bookstore-api.py

--- bookstore-api.py
+++ bookstore-api.py
@@ -104,7 +104,7 @@ def remove_book(book):
     return True if row is None else False
 
 
-# Write a function named `home` which returns 'Welcome to the Callahan's Bookstore API Service',
+# Write a function named `home` 
 # and assign to the static route of ('/')
 @app.route('/')
 def home():
